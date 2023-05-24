74f6a82d89a2c9fc4036f1e9d01ca34487eab897
hiteshnavghade75 <hiteshnavghade75@gmail.com>
2023-05-24 22:51:49 +0530

    added fs file

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs/promises')
+
+const myFileWriter = async (fileName, fileContent) => {
+	// write code here
+	// dont chnage function name
+	fs.writeFile("file.txt", "Hello");
+}
+
+const myFileReader = async (fileName) => {
+	// write code here
+	// dont chnage function name
+	fs.readFile("file.txt");
+}
+
+const myFileUpdater = async (fileName, fileContent) => {
+	// write code here
+	// dont chnage function name
+	fs.appendFile("file.txt", " World");
+}
+
+const myFileDeleter = async (fileName) => {
+	// write code here
+	// dont chnage function name
+	fs.unlink("file.txt");
+}
+
+// myFileWriter()
+// myFileReader()
+// myFileUpdater()
+// myFileDeleter()
+
+module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }
